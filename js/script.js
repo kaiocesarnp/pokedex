86f1c5b6f98d347d133a0eb69e5586af868ab3c6
kaiocesarnp <kaiocesarnp@gmail.com>
2022-10-02 23:59:45 -0300

    atualização

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 const fetchPokemon = async (pokemon) => {  //'async' = função assincrona
-    const APIResponse = await fetch (`https://pokeapi.co/api/v2/pokemon/${pokemon}`); //'await' espera o carregamento da api concluir, ao invés de passar para as próximas. 'await' só pode ser usado em funções assíncronas   
-                                                                                                                     
-    console.log(APIResponse);
+    const APIResponse = await fetch (`https://pokeapi.co/api/v2/pokemon/${pokemon}`); //'await' espera o carregamento da api concluir, ao invés de passar para as próximas. 'await' só pode ser usado em funções assíncronas
+    const data = await APIResponse.json(); //extrair repostas da api em JSON. Função assincrona
+    return data;
 }
 
-fetchPokemon();
+//função q renderiza os dados na tela
+const renderPokemon = async (pokemon) => {
+    const data =  await fetchPokemon(pokemon);
+}
 
