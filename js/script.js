584fa3c6ab224317b433910750d506dde71eedd9
kaiocesarnp <kaiocesarnp@gmail.com>
2022-10-02 23:52:15 -0300

    atualização pokedex

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-console.log();
+const fetchPokemon = async (pokemon) => {  //'async' = função assincrona
+    const APIResponse = await fetch (`https://pokeapi.co/api/v2/pokemon/${pokemon}`); //'await' espera o carregamento da api concluir, ao invés de passar para as próximas. 'await' só pode ser usado em funções assíncronas   
+                                                                                                                     
+    console.log(APIResponse);
+}
+
+fetchPokemon();
+
